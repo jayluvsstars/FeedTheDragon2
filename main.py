104676e29f09fb1cf5a76619f24ae9b208b34090
Jalynn Bolds <boldsjal000@warren.k12.in.us>
2024-09-18 08:39:51 -0400

    added game assets

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 
-#Set display surface
+# Set display surface
 pygame.init()
 
-#Set display surface
+# Set display surface
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -22,23 +22,27 @@ COIN_ACCELERATION, 0.5
 BUFFER_DISTANCE, 100
 '''
 PLAYER_STARTING_LIVES = 5
+#YOU do the remaining 4 CONSTANTS
 
-
-PLAYER_STARTING_LIVES = 5
-'''create a variable that tracks and score and give it a staring value of 0 '''
-''' create a variable that tracks the player lives and set it equal to the constant that stores player starting lives '''
-''' create a variable that tracks the coin velocity and set it equal to the constant that coin starting velocity '''
+#Set Game Variables:  variable_name
+''' 3 variables
+score, 0
+player_lives, PLAYER_STARTING_LIVES
+coin_velocity, COIN_STARTING_VELOCITY
+'''
+score = 0
+#YOU do the remaining 2 variables
 
 #Set colors
 GREEN = (0, 255, 0)
-DARK_GREEN = (10, 50, 10)
+DARKGREEN = (10, 50, 10)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 #Set fonts
 font = pygame.font.Font('AttackGraffiti.ttf', 32)
 
-#Sets the text for the sorce
+#Set Text for Score
 '''
 variable names:  score_text, score_rect
 render text: "Score: " + str(score)
@@ -47,51 +51,102 @@ color: GREEN
 background: DARKGREEN
 rect location: topleft = (10, 10)  
 '''
-score_text = font.render("Score: " + str(10), True, GREEN, DARK_GREEN)
+score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10, 10)
 
-#Sets the text for lives
+#Set Text for Title (Similar to Score)
 '''
-variable names: lives_text, lives_rect
-render text: "Lives: " + str(player_lives)
-True
+variable names:  title_text , title_rect 
+render text: "Feed the Dragon"
+antialias: True
 color: GREEN
-background: DARKGREEN
-rect location: y = 10
+background: WHITE
+rect location: centerx = WINDOW_WIDTH//2
+rect location: y = 10 
 '''
 
-#Sets the text for game over
+
+#Set Text for Lives (Similar to Score)
 '''
-Variable: game_over_text
-Rect: game_over_rect
-PHRASE: "GAMEOVER" ,
-Antialias: True
-Color: GREEN,
-Background: DARKGREEN,
-Position: center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2),
+variable names:  lives_text, lives_rect
+render text: "Lives: " + str(player_lives)
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: y = 10  
 '''
+
+
+#Set Text for Game Over (Similar to Score)
+'''
+variable names:  game_over_text , game_over_rect 
+render text: "GAMEOVER"
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) 
+'''
+
+
+
 #Set Text for Continue (Similar to Score)
 '''
-variable names: continue_text, continue_rect
+variable names:  continue_text, continue_rect  
 render text: "Press any key to play again"
-True
+antialias: True
 color: GREEN
 background: DARKGREEN
 rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 32)
 '''
 
-#The main game loop
+
+
+
+#Set the sound and music
+coin_sound = pygame.mixer.Sound("coin_sound.wav")
+miss_sound = pygame.mixer.Sound("miss_sound.wav")
+miss_sound.set_volume(0.1)
+pygame.mixer.music.load("ftd_background_music.wav")
+
+
+
+'''
+variable names:  player_image, player_rect
+image source: "dragon_right.png"
+rect location: left = 32
+rect loaction: centery = WINDOW_HEIGHT // 2
+'''
+player_image = pygame.image.load("dragon_right.png")
+player_rect = player_image.get_rect()
+player_rect.left = 32
+player_rect.centery = WINDOW_HEIGHT // 2
+
+'''
+variable names: coin_image, coin_rect
+image source: "coin.png"
+rect location: x = WINDOWS_WIDTH + BIFFER_DISTANCE
+rect location: y = random.rendint(64, WINDOW_HEIGHT - 32
+'''
+
+# The main game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-#Fill the display
-display_surface.fill(BLACK)
+    #Fill the display
+    display_surface.fill(BLACK)
 
-display_surface.blit(score_text, score_rect)
+    display_surface.blit(score_text, score_rect)
+    ''' YOU DO:  blit title_text and title_rect'''
+    ''' YOU DO:  blit lives_text and lives_rect'''
+    ''' YOU DO:  blit "dragon_right.png" and player_rect'''
+    ''' YOU DO:  blit "coin.png" and coin_rect'''
 
+    #Update display and tick the clock
+    pygame.display.update()
+    clock.tick(FPS)
 
 pygame.quit()
